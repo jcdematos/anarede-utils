022b9bffd0d403aca70b81ab571e1517319861f6
Julio Cesar Matos <jc.de.mts@gmail.com>
2021-06-20 18:32:20 -0300

    Atualizado a classe Bar e adicionado chamada a readREL.

--- readPWF.py
+++ readPWF.py
@@ -54,72 +54,92 @@ class Bar:
 	hisPath = ""
 	files = [relPath, hisPath]
 	def __init__(self, number, estado, tipo, grupoBase, nome, area):
-		self.number = number
-		self.estado = estado
-		self.tipo = tipo
-		self.grupoBase = grupoBase
-		self.nome = nome
-		self.area = area
+		self.number = number.strip(" ")
+		self.estado = estado.strip(" ")
+		self.tipo = tipo.strip(" ")
+		self.grupoBase = grupoBase.strip(" ")
+		self.nome = nome.strip(" ")
+		self.area = area.strip(" ")
+
+
+	def addFluxPot(self, pg, qg, pl, ql):
+		self.PG = pg.strip(" ")
+		self.QG = qg.strip(" ")
+		self.PL = pl.strip(" ")
+		self.QL = ql.strip(" ")
+
 	def printBar(self):
 		print(self.number + self.estado + self.tipo + self.grupoBase + self.nome + self.area)
+		print(self.PG + self.QG + self.PL + self.QL)
 
 def printBars():
 	print(Bar.caseTitle, end="")
 	print(Bar.hisPath, end="")
 	print(Bar.relPath, end="")
 
+def readPWF():
+	pass
+
+
 myBars = []
 
-# Make directory structure
-file = openFile(sys, "case.pwf")
-print("Reading file : %s" % file)
-pathAnarede = anaredePath()
-print("Anarede path is " + pathAnarede)
-pathWork = workPath(file)
-print("Working Directory path is " + pathWork)
+def main():
+	# Make directory structure
+	file = openFile(sys)
+	print("Reading file : %s" % file)
+	pathAnarede = anaredePath()
+	print("Anarede path is " + pathAnarede)
+	pathWork = workPath(file)
+	print("Working Directory path is " + pathWork)
 
-start_time = time.time()
-#------------------------------------------------------------------------------
+	start_time = time.time()
+	#------------------------------------------------------------------------------
 
-fLines = readFile(file)
-#------------------------------------------------------------------------------
-for entrie in range(0, len(fLines)):
-	line = fLines[entrie]
-	if (line.startswith("TITU")):
-		# Salva nome do título do caso
-		Bar.caseTitle = fLines[entrie+1].rstrip("\n")
-		# Later use rstrip to remove new line and spaces
-		continue
-	if (line.startswith("DBAR")):
-		# Get basic information from bars
-		while True:
-			entrie += 1
-			line = fLines[entrie]
-			if (line.startswith("99999")):
-				break
-			if (line.startswith("(")):
-				continue
-			else:
-				Bar.nBars += 1
-				number = line[0:5]
-				estado = line[6]
-				tipo = line[7]
-				grupoBase = line[8:10]
-				nome = line[10:22]
-				area = line[73:76]
-				myBars.append(Bar(number,estado,tipo,grupoBase,nome,area))
-	if (line.startswith("ULOG")):
-		if (fLines[entrie+2].rfind("rel") != -1):
-			Bar.relPath = fLines[entrie+2].rstrip("\n")
-		if (fLines[entrie+2].rfind("his") != -1):
-			Bar.hisPath = fLines[entrie+2].rstrip("\n")
+	fLines = readFile(file)
+	#------------------------------------------------------------------------------
+	for entrie in range(0, len(fLines)):
+		line = fLines[entrie]
+		if (line.startswith("TITU")):
+			# Salva nome do título do caso
+			Bar.caseTitle = fLines[entrie+1].rstrip("\n")
+			# Later use rstrip to remove new line and spaces
+			continue
+		if (line.startswith("DBAR")):
+			# Get basic information from bars
+			while True:
+				entrie += 1
+				line = fLines[entrie]
+				if (line.startswith("99999")):
+					break
+				if (line.startswith("(")):
+					continue
+				else:
+					Bar.nBars += 1
+					number = line[0:5]
+					estado = line[6]
+					tipo = line[7]
+					grupoBase = line[8:10]
+					nome = line[10:22]
+					area = line[73:76]
+					myBars.append(Bar(number,estado,tipo,grupoBase,nome,area))
+		if (line.startswith("ULOG")):
+			if (fLines[entrie+2].rfind("rel") != -1):
+				Bar.relPath = fLines[entrie+2].rstrip("\n")
+			if (fLines[entrie+2].rfind("his") != -1):
+				Bar.hisPath = fLines[entrie+2].rstrip("\n")
 
-#------------------------------------------------------------------------------
-result = runAnarede(file)
-files = [Bar.relPath, Bar.hisPath]
-moveFiles(pathAnarede, pathWork, files)
+	#------------------------------------------------------------------------------
+	#result = runAnarede(file)
+	#files = [Bar.relPath, Bar.hisPath]
+	#moveFiles(pathAnarede, pathWork, files)
 
-print("Barras lidas")
-for bar in myBars:
-	Bar.printBar(bar)
-print("--- %s seconds ---" % (time.time() - start_time))
+
+	readREL(myBars, pathWork+"/"+Bar.relPath)
+
+	print("Barras lidas")
+	for bar in myBars:
+		Bar.printBar(bar)
+	print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+	main()
