7199abf930a06c14ae71c4cebbf8e06c880d8000
Julio Cesar Matos <jc.de.mts@gmail.com>
2021-06-21 17:18:54 -0300

    Moved functions from readPWF.py

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
-
 import sys
 import os
+import subprocess
+from pathlib import Path
 
 import tkinter as tk
 from tkinter import filedialog
@@ -24,3 +25,34 @@ def readFile(file):
 	with open(file, 'r') as dados:
 		lines = dados.readlines();
 	return lines
+
+def anaredePath(config="anaredePath.config"):
+	""" Open config file and get anarede path """
+	path = readFile(config)[0].strip("\n")
+	if os.path.exists(path):
+		return path
+	print("Path doesnt exists")
+	return False
+
+def workPath(file):
+	fileStart = file.rfind("/")
+	return file[0:fileStart]
+
+def cleanHeader():
+	""" Function to clear the headers created by anarede """
+	pass
+
+def runAnarede(file):
+	""" Runs anarede with the given file """
+	return subprocess.run(["anarede", file], stdout=subprocess.PIPE, text=True)
+
+def moveFiles(anaPath, workPath, filesList, mkdir=False, dirName="tempDir"):
+	""" Move files from anarede work dir to the desired work dir """
+	anaPath = anaPath
+	workPath = workPath
+	for file in filesList:
+		scr = anaPath+"/"+file
+		dst = workPath+"/"+file
+		if os.path.exists(dst): # Delete file if already exists
+			Path(dst).unlink(True)
+		Path(scr).rename(dst)
