472e7d982e1a669879a149696640eb8f9da835e2
Julio Cesar Matos <jc.de.mts@gmail.com>
2021-06-21 17:18:36 -0300

    Moved functions to functions.py

--- readPWF.py
+++ readPWF.py
@@ -8,43 +8,9 @@ BUGS -
 DESCRIPTION - Programa que le o pwf de um caso base do ANAREDE e remove dados
 			  relevantes para analisar a estabilidade de tensão.
 """
-import re
 import time
-import subprocess
-from pathlib import Path
 from functions import *
 from readREL import *
-
-def anaredePath(config="anaredePath.config"):
-	""" Open config file and get anarede path """
-	path = readFile(config)[0]
-	if os.path.exists(path):
-		return path
-	print("Path doesnt exists")
-	return False
-
-def workPath(file):
-	fileStart = file.rfind("/")
-	return file[0:fileStart]
-
-def cleanHeader():
-	""" Function to clear the headers created by anarede """
-	pass
-
-def runAnarede(file):
-	""" Runs anarede with the given file """
-	return subprocess.run(["anarede", file], stdout=subprocess.PIPE, text=True)
-
-def moveFiles(anaPath, workPath, filesList, mkdir=False, dirName="tempDir"):
-	""" Move files from anarede work dir to the desired work dir """
-	anaPath = anaPath
-	workPath = workPath
-	for file in filesList:
-		scr = anaPath+"/"+file
-		dst = workPath+"/"+file
-		if os.path.exists(dst): # Delete file if already exists
-			Path(dst).unlink(True)
-		Path(scr).rename(dst)
 
 class Bar:
 	""" Class to hold data for the bars """
@@ -129,9 +95,9 @@ def main():
 				Bar.hisPath = fLines[entrie+2].rstrip("\n")
 
 	#------------------------------------------------------------------------------
-	#result = runAnarede(file)
-	#files = [Bar.relPath, Bar.hisPath]
-	#moveFiles(pathAnarede, pathWork, files)
+	result = runAnarede(file)
+	files = [Bar.relPath, Bar.hisPath]
+	moveFiles(pathAnarede, pathWork, files)
 
 
 	readREL(myBars, pathWork+"/"+Bar.relPath)
