cd35d1cbc20e8f10879de325013f9798d43755ba
Julio Cesar Matos <jc.de.mts@gmail.com>
2021-06-21 22:19:05 -0300

    Creates random growth factors and save in folders.

--- fpGen.py
+++ fpGen.py
@@ -10,18 +10,16 @@ DESCRIPTION - Cria casos de fluxo de potência continuado com diferentes fatores
 			  de crescimento.
 """
 import time
+import random
 from pathlib import Path
 from functions import *
 from random import seed
-from random import random
 
-seed(time.time())
+random.seed(time.time())
 
 def findDINC(fLines):
 	""" Recebe linhas do arquivo de interesse e retorna o index da linha onde
-	define-se o crescimento da potência ativa e reativa.
-	"""
-
+	define-se o crescimento da potência ativa e reativa. """
 	for entrie in range(0, len(fLines)):
 		line = fLines[entrie]
 		if (line.startswith("DINC")):
@@ -34,11 +32,45 @@ def findDINC(fLines):
 					break;
 				return entrie
 
+def genCPFLOW(fLines, nLinha):
+	""" Generates a new growth factor and returns a new list of lines using that
+	growth factor and the growth power factor"""
+	# Make it possible to limit by an uppper and lower bound
+	p = '{:<05}'.format(random.randrange(0,100)/1000)
+	q = '{:<05}'.format(random.randrange(0,100)/1000)
+	newGrowth = p + " " + q + "\n"
+	fLines[nLinha] = fLines[nLinha][0:52]+newGrowth
+	# potencia aparente
+	s=(float(p)*2+float(q)**2)**(1/2)
+	# fator de potencia
+	if float(q) != 0:
+		fp = '{0:.03}'.format(float(p)/s)
+	else:
+		fp = '{0:.03}'.format(1.0)
+	print('p ' + p + " q " + q + " fp " + fp)
+	return [fLines, fp.replace(".", "")]
+
+def saveCPFLOWpwf(fLines, fp, workPath):
+	""" Get the file and the new growth factor and save in the work path inside
+	a new folder """
+	fileFolder =workPath+"/cpflow_"+fp
+	if Path(fileFolder).exists():
+		return False
+	Path(fileFolder).mkdir()
+	filePath = fileFolder+"/cpflow_"+fp+".pwf"
+
+	file = open(filePath, "w")
+	file.writelines(fLines)
+	file.close()
+	return True
+
 file = openFile(sys)
 fLines = readFile(file)
+nLinha = findDINC(fLines)
+pathWork = workPath(file)
 
-linha = findDINC(fLines)
-
-f = open("teste.pwf", "w")
-f.writelines(fLines[linha])
-f.close()
+contador = 0
+while contador < 200:
+	[newfLines, newFP] = genCPFLOW(fLines, nLinha)
+	if saveCPFLOWpwf(newfLines, newFP, pathWork):
+		contador+=1
