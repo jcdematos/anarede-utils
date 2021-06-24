7e00e34ee6ce7fe01a6bdd4963296b8e39f63517
Julio Cesar Matos <jc.de.mts@gmail.com>
2021-06-24 19:59:16 -0300

    Organização do código.

--- readREL.py
+++ readREL.py
@@ -1,33 +1,30 @@
 """
 PROGRAM NAME - readREL
 PROGRAMMER - J. Matos, 06-2021
-USAGE - Run the program after running readPWF in a PFLOW simulation file.
 DATE - 19/06/2021
 BUGS -
-DESCRIPTION - Le um relatório gerado pelo anarede e salva o módulo de tensão,
-              angulo da barra e dados de geração e dados de carga em cada barra.
+DESCRIPTION - Function to read a .REL file.
 """
-import re
-import time
-import subprocess
-from pathlib import Path
 from functions import *
 
 def readREL(myBars, file="none"):
-    print(file)
+    """ Read a .REL file and add data to a Bar object """
     file = openFile(sys, file)
     fLines = readFile(file)
 
     for entrie in range(0, len(fLines)):
         line = fLines[entrie]
         if (line.find("RELATORIO DE BARRAS CA") > -1):
+            # Inicia leitura de pagina
             entrie+=8 # Pula cabeçalho
             while True:
                 line = fLines[entrie]
                 entrie+=1
                 if (line.find("CEPEL") > -1):
+                    # Para leitura no fim da página
                     break
                 else:
+                    # Le dados de geração da barra
                     number = line[3:8].strip(" ")
                     for bar in myBars:
                         if (bar.number == number):
